be3fb0a4a33cb478bf879cb10118a87ee6b44d0f
Silvano Cerza <silvanocerza@gmail.com>
2019-06-28 16:55:44 +0200

    Fixed some issues

--- saver.py
+++ saver.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Set
 
 import spotipy
 import spotipy.util as util
@@ -13,17 +13,16 @@ class Client:
         self.client = spotipy.Spotify(auth=token)
         self.album_file = "album_ids"
 
-    def current_user_saved_albums_contains(self, albums=None):
-        return self.client._get("me/albums/contains?ids=" + ",".join(albums))
+    def current_user_saved_albums_contains(self, album_id: List[str]):
+        return self.client._get(f"me/albums/contains?ids={','.join(album_id)}")
 
-    def get_albums(self) -> List[str]:
-        album_ids = []
+    def get_albums(self) -> Set[str]:
+        album_ids = set()
 
         if os.path.isfile(self.album_file):
             with open(self.album_file) as f:
-                album_ids = [i for i in f.readlines()]
-            print("Ids read from file")
-            return album_ids
+                print("Ids read from file")
+                return [i.strip() for i in f.readlines()]
 
         offset = 0
         limit = 50
@@ -33,7 +32,7 @@ class Client:
             tracks = self.client.current_user_saved_tracks(offset=offset, limit=limit)
 
             for item in tracks.get("items"):
-                album_ids.append(item["track"]["album"]["id"])
+                album_ids.add(item["track"]["album"]["id"])
 
             offset += limit
             page += 1
@@ -43,26 +42,25 @@ class Client:
 
         self.write_to_file(album_ids)
 
-    def write_to_file(self, album_ids: List[str]) -> None:
+    def write_to_file(self, album_ids: Set[str]) -> None:
         # Write the album ids to file just to be sure
         with open(self.album_file, "w") as f:
             for i in album_ids:
                 f.write(i)
                 f.write("\n")
 
-    def save_albums(self, album_ids: List[str]) -> None:
-        mask = self.current_user_saved_albums_contains(album_ids)
-
-        # Filter out those already saved in library and invert the result
-        to_add = [i for i, m in zip(album_ids, mask) if not m]
-        to_add = to_add[::-1]
-
-        self.client.current_user_saved_albums_add(to_add)
+    def save_albums(self, album_ids: Set[str]) -> None:
+        steps = 50
+        for n in range(0, len(album_ids), steps):
+            print(f"Saving albums from {n} to {n+steps}")
+            ids = album_ids[n : n + steps]
+            self.client.current_user_saved_albums_add(ids)
 
 
 if __name__ == "__main__":
     c = Client()
 
     albums = c.get_albums()
-    c.save_albums(album_ids=albums)
+    # Save albums in reverse order
+    c.save_albums(album_ids=albums[::-1])
 
